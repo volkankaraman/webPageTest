f829f20693e6d262e37b02a296c7ec4299479ae8
volkankaraman <developer@gmail.com>
2019-05-15 23:26:31 +0300

    yeni güncelleme

--- webPageTest/src/test/java/wepPageTester/webPageTest/NewTest.java
+++ webPageTest/src/test/java/wepPageTester/webPageTest/NewTest.java
@@ -2,35 +2,45 @@ package wepPageTester.webPageTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
  
 public class NewTest {
  
- public WebDriver driver;
+ public WebDriver driver; 
  
-  @Test
-  public void openMyBlog() {
- driver.get("https://www.softwaretestingmaterial.com/");
-  }
   
-  @BeforeClass
-  public void beforeClass() {
-   
-   //System.setProperty("webdriver.gecko.driver", "D:\\SeleniumDrivers\\geckodriver.exe");
-   //driver = new FirefoxDriver();
-   System.setProperty("webdriver.chrome.driver", "D:\\SeleniumDrivers\\chromedriver.exe");
-   driver = new ChromeDriver();
-   //denemef
-   //fghdfghfh
-   System.out.println("deneme.sayfa açıldı");
-  }
- 
-  @AfterClass
-  public void afterClass() {
-   driver.quit();
-  }
- 
+public class TestChromeBrowser {
+	
+	static final String driverPath = "D:\\SeleniumDrivers\\";
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		System.out.println("*******************");
+		System.out.println("launching chrome browser");
+		System.setProperty("webdriver.chrome.driver", driverPath+"chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void testGooglePageTitleInIEBrowser() {
+		driver.navigate().to("http://www.google.com");
+		String strPageTitle = driver.getTitle();
+		System.out.println("Page title: - "+strPageTitle);
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing chrome browser");
+			driver.quit();
+		}
+	}
+	
+}
 }
