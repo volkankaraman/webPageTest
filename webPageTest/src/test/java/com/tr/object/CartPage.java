b09df0df28a53f08f1708f54dc1eb14b58ad07d7
volkankaraman <developer@gmail.com>
2019-05-25 01:38:26 +0300

    Gereksizler Çıkarıldı

--- webPageTest/src/test/java/com/tr/object/CartPage.java
+++ webPageTest/src/test/java/com/tr/object/CartPage.java
@@ -1,6 +1,6 @@
 package com.tr.object;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- webPageTest/src/test/java/com/tr/object/HomePage.java
+++ webPageTest/src/test/java/com/tr/object/HomePage.java
@@ -2,13 +2,10 @@ package com.tr.object;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends PageObject {
 
